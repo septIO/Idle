43c0fc5ebcb57266326cd0ea38e4b90ba4b66007
septIO <in@sept.io>
2015-08-21 23:22:28 +0200

    Added menu

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
 angular.module('idle', [])
 
 
-.controller('MainController', ['$scope', function($scope){
+.controller('MainController', ['$scope', '$rootScope', function(scope, RS){
   
   
   
   
 }]) // Main Controller
+
+.controller('MenuController', ['$scope', '$rootScope', function(scope, RS){
+  
+  scope.menus = [
+    {
+      name : 'Machines'
+    },
+    {
+      name : 'Upgrades'
+    }
+  ];
+  
+  RS.currentMenu = scope.menus[0];
+  
+  scope.goToMenu = function(menu){
+    RS.currentMenu = menu;
+  }
+  
+}]) // Menu Controller
